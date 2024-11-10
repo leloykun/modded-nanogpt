052c022d6a4387e06080930c3d22d14999a64832
Franz Louis Cesista <franzlouiscesista@gmail.com>
2024-11-11 05:18:26 +0800

    .

--- train_gpt2.py
+++ train_gpt2.py
@@ -197,12 +197,11 @@ class MLP(nn.Module):
         self.c_proj  = CastedLinear(4 * config.n_embd, config.n_embd, bias=False)
         self.c_proj.weight.data.zero_() # zero init suggested by @Grad62304977
         torch.nn.init.orthogonal_(self.c_fc.weight.data)
-        self.c_scalars = nn.Parameter(torch.tensor([0.5, 2.0]))
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        x = self.c_fc(x) * self.c_scalars[0]
+        x = self.c_fc(x) * torch.tensor(0.5, dtype=x.dtype, device=x.device)
         x = F.relu(x).square() # https://arxiv.org/abs/2109.08668v2; ~1-2% better than GELU; suggested by @SKYLINEZ007 and @Grad62304977
-        x = self.c_proj(x) * self.c_scalars[1]
+        x = self.c_proj(x) * torch.tensor(2.0, dtype=x.dtype, device=x.device)
         return x
 
 class Block(nn.Module):
