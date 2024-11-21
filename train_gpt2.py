89cbb54c62646103ec24d73c1bb98012e99388f4
Franz Louis Cesista <franzlouiscesista@gmail.com>
2024-11-21 17:10:23 +0800

    .

--- train_gpt2.py
+++ train_gpt2.py
@@ -398,7 +398,7 @@ class Hyperparameters:
     # evaluation and logging hyperparams
     val_loss_every : int = 125 # every how many steps to evaluate val loss? 0 for only at the end
     # val_tokens : int = 10485760 # how many tokens of validation data? it's important to keep this fixed for consistent comparisons
-    val_tokens: int = 10420224
+    val_tokens: int = 10223616
     save_every : int = 0 # every how many steps to save the checkpoint? 0 for only at the end
 args = Hyperparameters()
 
