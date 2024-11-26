6e8178f385e10fea2703e41c07eedf79382295ff
Franz Louis Cesista <franzlouiscesista@gmail.com>
2024-11-26 09:54:23 +0800

    .

--- train_gpt1.py
+++ train_gpt1.py
@@ -591,26 +591,26 @@ for step in range(args.num_iterations + 1):
         val_loss /= val_steps
         # log val loss to console and to logfile
         print0(f'step:{step}/{args.num_iterations} val_loss:{val_loss:.4f} train_time:{training_time_ms:.0f}ms step_avg:{training_time_ms/(timed_steps-1):.2f}ms')
-        if master_process:
-            print("============== Weight norms: ==============")
-            with open(logfile, "a") as f:
-                f.write("============== Weight norms: ==============\n")
-                for name, p in model.named_parameters():
-                    if p.ndim != 2:
-                        continue
-                    if "transformer.wte" in name:
-                        l1_to_l2_norm = torch.norm(p.data.float(), p=2, dim=1).mean().item()
-                    frobenius_norm = torch.linalg.norm(p.data.float(), ord='fro').item()
-                    spectral_norm = torch.linalg.matrix_norm(p.data.float(), ord=2).item()
-                    nuclear_norm = torch.linalg.matrix_norm(p.data.float(), ord="nuc").item()
-                    if "transformer.wte" in name:
-                        print(f"{name = } | {l1_to_l2_norm = :.5f} | {frobenius_norm = :.5f} | {spectral_norm = :.5f} | {nuclear_norm = :.5f}")
-                        f.write(f"{name = } | {l1_to_l2_norm = :.5f} | {frobenius_norm = :.5f} | {spectral_norm = :.5f} | {nuclear_norm = :.5f}\n")
-                    else:
-                        print(f"{name = } | {frobenius_norm = :.5f} | {spectral_norm = :.5f} | {nuclear_norm = :.5f}")
-                        f.write(f"{name = } | {frobenius_norm = :.5f} | {spectral_norm = :.5f} | {nuclear_norm = :.5f}\n")
-                f.write("===========================================\n")
-            print("===========================================")
+        # if master_process:
+        #     print("============== Weight norms: ==============")
+        #     with open(logfile, "a") as f:
+        #         f.write("============== Weight norms: ==============\n")
+        #         for name, p in model.named_parameters():
+        #             if p.ndim != 2:
+        #                 continue
+        #             if "transformer.wte" in name:
+        #                 l1_to_l2_norm = torch.norm(p.data.float(), p=2, dim=1).mean().item()
+        #             frobenius_norm = torch.linalg.norm(p.data.float(), ord='fro').item()
+        #             spectral_norm = torch.linalg.matrix_norm(p.data.float(), ord=2).item()
+        #             nuclear_norm = torch.linalg.matrix_norm(p.data.float(), ord="nuc").item()
+        #             if "transformer.wte" in name:
+        #                 print(f"{name = } | {l1_to_l2_norm = :.5f} | {frobenius_norm = :.5f} | {spectral_norm = :.5f} | {nuclear_norm = :.5f}")
+        #                 f.write(f"{name = } | {l1_to_l2_norm = :.5f} | {frobenius_norm = :.5f} | {spectral_norm = :.5f} | {nuclear_norm = :.5f}\n")
+        #             else:
+        #                 print(f"{name = } | {frobenius_norm = :.5f} | {spectral_norm = :.5f} | {nuclear_norm = :.5f}")
+        #                 f.write(f"{name = } | {frobenius_norm = :.5f} | {spectral_norm = :.5f} | {nuclear_norm = :.5f}\n")
+        #         f.write("===========================================\n")
+        #     print("===========================================")
         # start the clock again
         torch.cuda.synchronize()
         t0 = time.time()
