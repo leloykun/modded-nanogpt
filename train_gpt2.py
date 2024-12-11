7746ed7fffaa13c34037f2ca233fdc9ceb37905b
Franz Louis Cesista <franzlouiscesista@gmail.com>
2024-12-11 22:17:21 +0800

    .

--- train_gpt2.py
+++ train_gpt2.py
@@ -427,7 +427,7 @@ class Hyperparameters:
     val_loss_every : int = 125 # every how many steps to evaluate val loss? 0 for only at the end
     val_tokens : int = 10485760 # how many tokens of validation data? it's important to keep this fixed for consistent comparisons
     save_every : int = 0 # every how many steps to save the checkpoint? 0 for only at the end
-    log_norms: bool = False # log the norms of the weights and gradients
+    log_norms: bool = True # log the norms of the weights and gradients
 args = Hyperparameters()
 
 # set up DDP (distributed data parallel). torchrun sets this env variable
