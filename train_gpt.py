b13f9c1cd1a1285ed910a7b1d2dd8ffd0217ad5d
Franz Louis Cesista <franzlouiscesista@gmail.com>
2025-01-24 20:29:41 +0800

    .

--- train_gpt.py
+++ train_gpt.py
@@ -473,7 +473,7 @@ class Hyperparameters:
     # evaluation and logging
     val_loss_every = 125 # every how many steps to evaluate val loss? 0 for only at the end
     # implementation
-    seq_len = 96*1024 # FlexAttention sequence length
+    seq_len = 48*1024 # FlexAttention sequence length
     val_seq_len = 64*1024 # FlexAttention sequence length for validation
     save_checkpoint = False
 
