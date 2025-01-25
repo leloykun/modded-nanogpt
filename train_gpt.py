e677ff009f9846207cf86b4b05eefc8245be09a0
Franz Louis Cesista <franzlouiscesista@gmail.com>
2025-01-26 03:26:36 +0800

    .

--- train_gpt.py
+++ train_gpt.py
@@ -296,7 +296,7 @@ class CausalSelfAttention(nn.Module):
 class MLP(nn.Module):
     def __init__(self, dim: int):
         super().__init__()
-        self.c_fc = CastedLinear(dim, 4 * dim)
+        self.c_fc = CastedLinear(dim, 4 * dim, use_fp8=True, x_scale=2.0, w_scale=2.0**8, grad_scale=2.0**23)
         self.c_proj = CastedLinear(4 * dim, dim)
         self.c_proj.weight.detach().zero_() # zero init suggested by @Grad62304977
 
@@ -354,7 +354,7 @@ class GPT(nn.Module):
         self.skip_weights = nn.Parameter(torch.ones(self.num_decoder_layers))
         # there are only 50257 unique GPT-2 tokens; we extend to nearest multiple of 128 for efficiency.
         # suggested to me by @Grad62304977. this originates from Karpathy's experiments.
-        self.lm_head = CastedLinear(model_dim, next_multiple_of_n(vocab_size, n=128), use_fp8=True, x_scale=2.0, w_scale=2.0**9, grad_scale=2.0**20)
+        self.lm_head = CastedLinear(model_dim, next_multiple_of_n(vocab_size, n=128), use_fp8=True, x_scale=2.0, w_scale=2.0**8, grad_scale=2.0**19)
         self.lm_head.weight.detach().zero_() # @Grad62304977
 
     def forward(self, input_seq: Tensor, target_seq: Tensor, sliding_window_num_blocks: Tensor):
@@ -559,6 +559,10 @@ def train(args: Hyperparameters):
     t0 = time.perf_counter()
     # begin training
     train_steps = args.num_iterations
+    if master_process:
+        from collections import defaultdict
+        param_name_to_grad_scale_map = defaultdict(lambda: 1e9)
+        param_name_to_weight_scale_map = defaultdict(lambda: 1e9)
     for step in range(train_steps + 1):
         last_step = (step == train_steps)
         # This effectively ignores timing first 10 steps, which are slower for weird reasons.
@@ -619,6 +623,8 @@ def train(args: Hyperparameters):
                     weight_max_abs = param.abs().max().item()
                     grad_scale = np.ceil(np.log2(0.8 * 40896.0 / grad_max_abs)) if grad_max_abs > 1e-9 else 1
                     weight_scale = np.ceil(np.log2(0.8 * 448.0 / weight_max_abs)) if weight_max_abs > 1e-9 else 1
+                    param_name_to_grad_scale_map[name] = min(param_name_to_grad_scale_map[name], grad_scale)
+                    param_name_to_weight_scale_map[name] = min(param_name_to_weight_scale_map[name], weight_scale)
                     print0(f"{name:<40} | grad_scale={grad_scale:<3} | weight_scale={weight_scale:<3}", console=True)
         # momentum warmup for Muon
         frac = min(step / 300, 1)
@@ -633,6 +639,11 @@ def train(args: Hyperparameters):
         # logging
         approx_time = training_time_ms + 1000 * (time.perf_counter() - t0)
         print0(f"step:{step+1}/{train_steps} train_time:{approx_time:.0f}ms step_avg:{approx_time/timed_steps:.2f}ms", console=True)
+    if master_process:
+        for name, grad_scale in param_name_to_grad_scale_map.items():
+            print0(f"{name:<40} | grad_scale={grad_scale:<3}", console=True)
+        for name, weight_scale in param_name_to_weight_scale_map.items():
+            print0(f"{name:<40} | weight_scale={weight_scale:<3}", console=True)
 
     print0(
         f"peak memory allocated: {torch.cuda.max_memory_allocated() // 1024 // 1024} MiB "
