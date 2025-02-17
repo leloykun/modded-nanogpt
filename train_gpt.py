71b2e770f80d427e2906a1554d24940a71a6b4ab
Franz Louis Cesista <franzlouiscesista@gmail.com>
2025-02-17 19:24:27 +0800

    .

--- train_gpt.py
+++ train_gpt.py
@@ -453,7 +453,7 @@ class Hyperparameters:
     train_seq_len = 48*1024 # FlexAttention sequence length
     val_seq_len = 4*64*1024 # FlexAttention sequence length for validation
     # optimization
-    num_iterations = 1750 # number of iterations to run
+    num_iterations = 1755 # number of iterations to run
     cooldown_frac = 0.4 # fraction of training spent cooling down the learning rate
     # architecture
     vocab_size = 50257
