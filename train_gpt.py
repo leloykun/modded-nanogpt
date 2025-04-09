529759c5310944088f90c63ccff3846b62a10a09
Franz Louis Cesista <franzlouiscesista@gmail.com>
2025-04-09 21:49:18 +0800

    test relu6

--- train_gpt.py
+++ train_gpt.py
@@ -292,7 +292,7 @@ class MLP(nn.Module):
 
     def forward(self, x: Tensor):
         x = self.c_fc(x)
-        x = F.relu(x).square() # https://arxiv.org/abs/2109.08668v2; ~1-2% better than GELU; suggested by @SKYLINEZ007 and @Grad62304977
+        x = F.relu6(x).square() # https://arxiv.org/abs/2109.08668v2; ~1-2% better than GELU; suggested by @SKYLINEZ007 and @Grad62304977
         x = self.c_proj(x)
         return x
 
